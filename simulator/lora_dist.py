f395d991685b7232aeeee4b732fbf31efc28fb4e
Jason Yan <jason.yan@berkeley.edu>
2024-04-02 18:46:27 -0700

    update size

--- simulator/lora_dist.py
+++ simulator/lora_dist.py
@@ -4,28 +4,36 @@ import json
 with open('cache/civit_catalog.json') as f:
     civit_catalog = json.load(f)
 
-# Create a dictionary to store LORA id and its downloadCount
+# Create a dictionary to store LORA id and its downloadCount and size
 lora_dist = {}
 for item in civit_catalog:
     if item['type'] == 'LORA':
-        lora_dist[item['id']] = item['stats']['downloadCount']
+        count = item['stats']['downloadCount']
+        if len(item['modelVersions']) == 0:
+            continue
+        if len(item['modelVersions'][0]['files']) == 0:
+            continue
+        size = item['modelVersions'][0]['files'][0]['sizeKB']
+        size_MB = size / 1024
+        lora_dist[item['id']] = {'downloadCount': count, 'sizeMB': size_MB}
 
 # Sort the dictionary by downloadCount
-lora_dist = dict(sorted(lora_dist.items(), key=lambda item: item[1], reverse=True))
+lora_dist = dict(sorted(lora_dist.items(), key=lambda item: item[1]['downloadCount'], reverse=True))
 
 # Save the dictionary to lora_dist.json
 with open('lora_dist.json', 'w') as f:
     json.dump(lora_dist, f, indent=4)
 
-# lora_info dictionary
-lora_info = {}
-for item in civit_catalog:
-    if item['type'] == 'LORA':
-        lora_info[item['id']] = item
 
-# Sort the dictionary by downloadCount
-lora_info = dict(sorted(lora_info.items(), key=lambda item: item[1]['stats']['downloadCount'], reverse=True))
+# # lora_info dictionary
+# lora_info = {}
+# for item in civit_catalog:
+#     if item['type'] == 'LORA':
+#         lora_info[item['id']] = item
 
-# Save the dictionary to lora_info.json
-with open('lora_info.json', 'w') as f:
-    json.dump(lora_info, f, indent=4)
+# # Sort the dictionary by downloadCount
+# lora_info = dict(sorted(lora_info.items(), key=lambda item: item[1]['stats']['downloadCount'], reverse=True))
+
+# # Save the dictionary to lora_info.json
+# with open('lora_info.json', 'w') as f:
+#     json.dump(lora_info, f, indent=4)
